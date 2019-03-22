cb577b3e2e474795d95b0d768f8cabfe3975fc29
Mario Hernando Segovia <mariohese@hotmail.com>
2019-03-22 10:52:02 +0100

    Cambios en routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/registro', function(req, res, next) {
+  res.render('registro', { title: 'Express' });
+});
+
 module.exports = router;
